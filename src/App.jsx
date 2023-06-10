4ab8af2775e9f11b33cdffead4606bea19140217
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2023-06-10 16:05:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Dashboard } from "./pages"
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Dashboard from "./Dashboard";
+import Register from "./Register";
+
+export{
+    Dashboard,
+    Register
+}
